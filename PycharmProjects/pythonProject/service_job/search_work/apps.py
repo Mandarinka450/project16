444fca72c6274d186e7f6ecc46539bce160b2c21
Mandarinka450 <nastya-s-01@mail.ru>
2021-01-13 21:50:22 +0300

    finish

--- PycharmProjects/pythonProject/service_job/search_work/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SearchWorkConfig(AppConfig):
-    name = 'search_work'
--- PycharmProjects/pythonProject/service_job/search_work/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'search_work/index.html')
